3f00dfaf2ea54b9be61438eec2d49457e507e1b9
jaigansa <jaigansa@gmail.com>
2025-02-18 11:59:43 +0530

    i tjink

--- themes/tamilnux/assets/js/theme-toggle.js
+++ themes/tamilnux/assets/js/theme-toggle.js
@@ -1,30 +1,5 @@
 console.log('This is toggle theme js');
 
-function toggleTheme() {
-  let currentTheme = document.documentElement.classList.contains("dark-theme") ? "light" : "dark";
-  setTheme(currentTheme);
-}
 
-function setTheme(theme) {
-  document.documentElement.classList.toggle("dark-theme", theme === "dark");
-  document.documentElement.classList.toggle("light-theme", theme === "light");
-
-  localStorage.setItem("theme", theme);
-
-  // Update toggle switch state
-  const toggleSwitch = document.getElementById("theme-toggle");
-  if (toggleSwitch) {
-    toggleSwitch.checked = theme === "dark";
-  }
-}
-
-function getPreferredTheme() {
-  return localStorage.getItem("theme") || 
-         (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTheme(getPreferredTheme());
-});
 
 
