d8a4911df84aec4d3d7ca71c1fde929eab2ca1ba
jaigansa <jaigansa@users.noreply.github.com>
2025-02-18 06:30:05 +0000

    deploy: 3f00dfaf2ea54b9be61438eec2d49457e507e1b9

---
+++ js/theme.js
@@ -0,0 +1,26 @@
+function toggleTheme() {
+    let currentTheme = document.documentElement.classList.contains("dark-theme") ? "light" : "dark";
+    setTheme(currentTheme);
+  }
+  
+  function setTheme(theme) {
+    document.documentElement.classList.toggle("dark-theme", theme === "dark");
+    document.documentElement.classList.toggle("light-theme", theme === "light");
+  
+    localStorage.setItem("theme", theme);
+  
+    // Update toggle switch state
+    const toggleSwitch = document.getElementById("theme-toggle");
+    if (toggleSwitch) {
+      toggleSwitch.checked = theme === "dark";
+    }
+  }
+  
+  function getPreferredTheme() {
+    return localStorage.getItem("theme") || 
+           (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    setTheme(getPreferredTheme());
+  });
